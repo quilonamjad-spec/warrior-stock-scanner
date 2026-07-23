72e1ca19ff150fa0123e04f1583ae8430be16e27
quilonamjad-spec <quilonamjad@gmail.com>
2026-07-23 09:34:12 +0530

    Update grokscaner.py

--- grokscaner.py
+++ grokscaner.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import yfinance as yf
 import pandas as pd
-import pandas_ta as ta
+import pandas-ta as ta
 import plotly.graph_objects as go
 
 st.set_page_config(page_title="Day Trading Scanner", layout="wide")
